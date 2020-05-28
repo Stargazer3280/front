bb622710ebea932a2533d18636472b650e13d724
Stargazer3280 <32805253@qq.com>
2020-05-28 20:20:25 +0800

    迭代四2

--- src/router/index.js
+++ src/router/index.js
@@ -12,114 +12,114 @@ const routes = [
     {
         path: '/upload',
         name: '上传',
-        component: () => import('../views/Upload.vue')
+        component: resolve => require(['../views/Upload'], resolve)
     },
     {
         path: '/information',
         name: '信息展示',
-        component: () => import('../views/Information.vue')
+        component: resolve => require(['../views/Information'], resolve)
     },
     ///////////////////////////////////////////////////////////////////
     {
         path: '/insert/civilStructure',
         name: '插入土木结构房屋破坏统计表',
-        component: () => import('../views/insert/CivilStructure')
+        component: resolve => require(['../views/insert/CivilStructure'], resolve)
     },
     {
         path: '/insert/collapseRecord',
         name: '插入人员死亡统计表',
-        component: () => import('../views/insert/CollapseRecord')
+        component: resolve => require(['../views/insert/CivilStructure'], resolve)
     },
     {
         path: '/insert/commDisaster',
         name: '插入通信系统灾情统计表  ',
-        component: () => import('../views/insert/CommDisaster')
+        component: resolve => require(['../views/insert/CommDisaster'], resolve)
     },
     {
         path: '/insert/deathStatistics',
         name: '插入人员死亡统计表',
-        component: () => import('../views/insert/DeathStatistics')
+        component: resolve => require(['../views/insert/DeathStatistics'], resolve)
     },
     {
         path: '/insert/disasterInfo',
         name: '插入灾情预测',
-        component: () => import('../views/insert/DisasterInfo')
+        component: resolve => require(['../views/insert/DisasterInfo'], resolve)
     },
     {
         path: '/insert/disasterPrediction',
         name: '插入灾情预测',
-        component: () => import('../views/insert/DisasterPrediction')
+        component: resolve => require(['../views/insert/DisasterPrediction'], resolve)
     },
     {
         path: '/insert/landslideRecord',
         name: '插入滑坡',
-        component: () => import('../views/insert/LandslideRecord')
+        component: resolve => require(['../views/insert/LandslideRecord'], resolve)
     },
     {
         path: '/insert/masonryStructure',
         name: '插入砖混结构房屋破坏',
-        component: () => import('../views/insert/MasonryStructure')
+        component: resolve => require(['../views/insert/MasonryStructure'], resolve)
     },
     {
         path: '/insert/missingStatistics',
         name: '插入失踪人数',
-        component: () => import('../views/insert/MissingStatistics')
+        component: resolve => require(['../views/insert/MissingStatistics'], resolve)
     },
     {
         path: '/insert/trafficDisaster',
         name: '插入交通',
-        component: () => import('../views/insert/TrafficDisaster')
+        component: resolve => require(['../views/insert/TrafficDisaster'], resolve)
     },
     ////////////////////////////////////////////////////////////////////
     {
         path: '/update/civilStructure',
         name: '更新土木结构房屋破坏统计表',
-        component: () => import('../views/update/CivilStructure')
+        component: resolve => require(['../views/update/CivilStructure'], resolve)
     },
     {
         path: '/update/collapseRecord',
         name: '更新人员死亡统计表',
-        component: () => import('../views/update/CollapseRecord')
+        component: resolve => require(['../views/update/CollapseRecord'], resolve)
     },
     {
         path: '/update/commDisaster',
         name: '更新通信系统灾情统计表  ',
-        component: () => import('../views/update/CommDisaster')
+        component: resolve => require(['../views/update/CommDisaster'], resolve)
     },
     {
         path: '/update/deathStatistics',
         name: '更新人员死亡统计表',
-        component: () => import('../views/update/DeathStatistics')
+        component: resolve => require(['../views/update/DeathStatistics'], resolve)
     },
     {
         path: '/update/disasterInfo',
         name: '更新灾情预测',
-        component: () => import('../views/update/DisasterInfo')
+        component: resolve => require(['../views/update/DisasterInfo'], resolve)
     },
     {
         path: '/update/disasterPrediction',
         name: '更新灾情预测',
-        component: () => import('../views/update/DisasterPrediction')
+        component: resolve => require(['../views/update/DisasterPrediction'], resolve)
     },
     {
         path: '/update/landslideRecord',
         name: '更新滑坡',
-        component: () => import('../views/update/LandslideRecord')
+        component: resolve => require(['../views/update/LandslideRecord'], resolve)
     },
     {
         path: '/update/masonryStructure',
         name: '更新砖混结构房屋破坏',
-        component: () => import('../views/update/MasonryStructure')
+        component: resolve => require(['../views/update/MasonryStructure'], resolve)
     },
     {
         path: '/update/missingStatistics',
         name: '更新失踪人数',
-        component: () => import('../views/update/MissingStatistics')
+        component: resolve => require(['../views/update/MissingStatistics'], resolve)
     },
     {
         path: '/update/trafficDisaster',
         name: '更新交通',
-        component: () => import('../views/update/TrafficDisaster')
+        component: resolve => require(['../views/update/TrafficDisaster'], resolve)
     },
 ]
 
